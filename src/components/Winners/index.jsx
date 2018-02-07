70bada21a785958ff68e698abd7b0f9d5356a149
lf7817 <535536456@qq.com>
2018-02-07 16:39:08 +0800

    fix some bugs:kissing:

--- src/components/Winners/index.jsx
+++ src/components/Winners/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { observable } from 'mobx'
 import { observer, inject } from 'mobx-react'
 import { TransitionGroup } from 'react-transition-group'
 import BounceInBottom from '../../base/BounceInBottom'
 import Winner from './winner'
 import PropTypes  from 'prop-types'
+import { message } from 'antd'
 
 import './style.css'
 
@@ -16,8 +18,18 @@ class Winners extends Component {
     store: PropTypes.object.isRequired
   }
 
+  @observable ulContainer = null
+
   reStart = () => {
-    this.props.store.reStart()
+    if (this.props.store.status === 1) {
+      message.error('抽奖过程中无法执行此操作！')
+    } else {
+      this.props.store.reStart()
+    }
+  }
+
+  scrollToBottom = () => {
+    this.ulContainer.scrollTo(0, 1000)
   }
 
   render() {
@@ -26,20 +38,22 @@ class Winners extends Component {
     return (
       <div className="app-winners" style={{height: this.props.height}}>
         <h2 className="app-winners-title">{`获奖名单(${winnerList.length})`}</h2>
-        <TransitionGroup component="ul" className="app-winners-list">
-          {
-            winnerList.map((item, index) => (
-              <BounceInBottom key={index} >
-                <Winner
-                  id={item.awardId} 
-                  name={item.name}
-                  pic={`https://q.qlogo.cn/g?b=qq&nk=${item.qq}&s=100`}
-                  awardImage={awardsList[item.awardId].pic}
-                  title={awardsList[item.awardId].title} />
-              </BounceInBottom> 
-            ))
-          }
-        </TransitionGroup>        
+        <ul className="app-winners-list" ref={ulContainer => this.ulContainer = ulContainer}>
+          <TransitionGroup>
+            {
+              winnerList.map((item, index) => (
+                <BounceInBottom key={index} onEntered={this.scrollToBottom}>
+                  <Winner
+                    id={item.awardId} 
+                    name={item.name}
+                    pic={`https://q.qlogo.cn/g?b=qq&nk=${item.qq}&s=100`}
+                    awardImage={awardsList[item.awardId].pic}
+                    title={awardsList[item.awardId].title} />
+                </BounceInBottom> 
+              ))
+            }
+            </TransitionGroup>  
+        </ul>
         <div className="app-winners-list-footer">
           <button style={{cursor: 'pointer'}} onClick={this.reStart}>重新抽奖</button>
         </div>
