72555f186d827e55c8ed2e04ed39b6ed93828c08
lf7817 <535536456@qq.com>
2019-01-26 16:51:05 +0800

    完成页面等比例缩放

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,7 +2,7 @@
  * @Author: lifan
  * @Date: 2019-01-26 09:29:42
  * @Last Modified by: lifan
- * @Last Modified time: 2019-01-26 14:41:14
+ * @Last Modified time: 2019-01-26 16:49:56
  */
 import React, { Component } from 'react';
 import { DESIGN_HEIGHT, DESIGN_WIDTH } from '../../constants';
@@ -14,7 +14,7 @@ export default class App extends Component {
    * 计算主要容器尺寸
    */
   public calcMainContainerSize = () => {
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       if (!this.$refApp.current) {
         return;
       }
@@ -23,29 +23,24 @@ export default class App extends Component {
       const bHeight = document.documentElement.clientHeight;
       const ratio = DESIGN_HEIGHT / DESIGN_WIDTH; // 设计稿比例
       let scale = 1;
-      let origin = 'center center';
       const wHeight = bWidth * ratio; // 期望高度
       const wWidth = bHeight / ratio; // 期望宽度
 
       // tslint:disable-next-line:prefer-conditional-expression
       if (bHeight < wHeight) { // 以真实高度为基准等比例缩放
         scale = bHeight / DESIGN_HEIGHT;
-        origin = '0 0';
       } else {
         scale = bWidth / DESIGN_WIDTH;
-        origin = '0 0';
       }
 
-      // if (bWidth < wWidth) {
-      //   scale = bWidth / DESIGN_WIDTH;
-      //   origin = '0 0';
-      // }
+      const rw = DESIGN_WIDTH * scale;
+      const rh = DESIGN_HEIGHT * scale;
 
+      this.$refApp.current.style.margin = `${(bHeight - rh) / 2}px ${(bWidth - rw) / 2}px`;
       this.$refApp.current.style.height = DESIGN_HEIGHT + 'px';
       this.$refApp.current.style.width = DESIGN_WIDTH + 'px';
-      this.$refApp.current.style.transformOrigin = origin;
       this.$refApp.current.style.transform = `scale(${scale})`;
-    }, 0);
+    });
   }
 
   public componentDidMount() {
