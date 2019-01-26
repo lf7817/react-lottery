af283527b945841be9511f01ebae1ad88448024c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-26 09:28:26 +0000

    Deploy lf7817/react-lottery to github.com/lf7817/react-lottery.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3baee7ca32419c8c44272672bd28a037.js"
+  "./precache-manifest.31d8787956d9802ca7d479d15a3e9df2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
