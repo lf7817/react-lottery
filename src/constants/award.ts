f816eac4e13461ef3e975a5070a4c168df736c76
lf7817 <535536456@qq.com>
2019-01-30 11:20:28 +0800

    把奖品图片放到public目录下，防止打版本号

--- src/constants/award.ts
+++ src/constants/award.ts
@@ -2,13 +2,13 @@
  * @Author: lifan
  * @Date: 2019-01-27 14:18:57
  * @Last Modified by: lifan
- * @Last Modified time: 2019-01-28 15:44:24
+ * @Last Modified time: 2019-01-30 11:11:35
  */
-import Img_0 from '../assets/images/award_0.jpg';
-import Img_1 from '../assets/images/award_1.jpg';
-import Img_2 from '../assets/images/award_2.jpg';
-import Img_3 from '../assets/images/award_3.jpg';
-import Img_4 from '../assets/images/award_4.jpg';
+const Img0 = `${process.env.PUBLIC_URL}/assets/images/award_0.jpg`;
+const Img1 = `${process.env.PUBLIC_URL}/assets/images/award_1.jpg`;
+const Img2 = `${process.env.PUBLIC_URL}/assets/images/award_2.jpg`;
+const Img3 = `${process.env.PUBLIC_URL}/assets/images/award_3.jpg`;
+const Img4 = `${process.env.PUBLIC_URL}/assets/images/award_4.jpg`;
 
 export type IAwardId = 0 | 1 | 2 | 3 | 4;
 
@@ -24,35 +24,35 @@ export interface IAward {
 const awards: IAward[] = [
   {
     id: 0,
-    image: Img_0,
+    image: Img0,
     name: '电子相册',
     sum: 2,
     title: '一等奖',
   },
   {
     id: 1,
-    image: Img_1,
+    image: Img1,
     name: '智能眼罩',
     sum: 2,
     title: '二等奖',
   },
   {
     id: 2,
-    image: Img_2,
+    image: Img2,
     name: '天猫精灵',
     sum: 10,
     title: '三等奖',
   },
   {
     id: 3,
-    image: Img_3,
+    image: Img3,
     name: '阿尔卑斯糖',
     sum: 20,
     title: '四等奖',
   },
   {
     id: 4,
-    image: Img_4,
+    image: Img4,
     name: '幸运奖',
     sum: 15,
     title: '幸运奖',
