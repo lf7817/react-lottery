3207bbb6925036c5bda5667677ef964f377ecac8
lf7817 <535536456@qq.com>
2019-02-02 21:54:33 +0800

    采用getSnapshotBeforeUpdate计算获奖名单scrollTop

--- src/components/Winner/index.tsx
+++ src/components/Winner/index.tsx
@@ -9,7 +9,7 @@ import QueueAnim from 'rc-queue-anim';
 import Texty from 'rc-texty';
 // tslint:disable-next-line:no-submodule-imports
 import 'rc-texty/assets/index.css';
-import React, { FunctionComponent, memo } from 'react';
+import React, { PureComponent } from 'react';
 import { IAwards } from '../../store/reducers/award';
 import { IWinner } from '../../store/reducers/winnerList';
 import styles from './style.module.scss';
@@ -21,39 +21,71 @@ interface IWinnerProps {
   exportHandler: () => void;
 }
 
-const Winner: FunctionComponent<IWinnerProps> = ({ award, list, resetHandler, exportHandler }) => {
-  const onEnd = debounce((e: any) => {
-    const parentNode = e.target.parentNode;
-    if (parentNode) {
-      parentNode.scrollTo(0, 20000);
+export default class Winner extends PureComponent<IWinnerProps> {
+  public readonly $refList: React.RefObject<HTMLDivElement> = React.createRef();
+
+  public getSnapshotBeforeUpdate(prevProps: IWinnerProps) {
+    if (this.props.list.length > prevProps.list.length) {
+      const oDiv = this.$refList.current;
+
+      if (oDiv) {
+        return oDiv.scrollHeight - oDiv.scrollTop;
+      }
+
+      return null;
     }
-  }, 500);
 
-  return (
-    <div className={styles.winner}>
-      <div className={styles.title}>
-        <span>获奖名单</span>
-        <span>{award.last} / {award.sum}</span>
-      </div>
-      <QueueAnim className={styles.list} onEnd={onEnd}>
-        {
-          list.map((item) => (
-            <div key={item.phone} className={styles.item} title={award.title}>
-              <div className={styles.left}>
-                <Texty type="flash" mode="smooth" className={styles.name}>{item.name}</Texty>
-                <Texty type="flash" mode="smooth" className={styles.awardTitle}>{award.name}</Texty>
+    return null;
+  }
+
+  public componentDidUpdate(nextProps: IWinnerProps, state: any, snapShop: number) {
+    if (snapShop !== null) {
+      const oDiv = this.$refList.current;
+
+      if (oDiv) {
+        oDiv.scrollTop = oDiv.scrollHeight - snapShop;
+      }
+    }
+  }
+
+  public componentDidMount() {
+    this.initListScrollTop();
+  }
+
+  public initListScrollTop() {
+    const oDiv = this.$refList.current;
+
+    if (oDiv) {
+      oDiv.scrollTop = oDiv.scrollHeight - oDiv.clientHeight;
+    }
+  }
+
+  public render() {
+    const { award, list, resetHandler, exportHandler } = this.props;
+    return (
+      <div className={styles.winner}>
+        <div className={styles.title}>
+          <span>获奖名单</span>
+          <span>{award.last} / {award.sum}</span>
+        </div>
+        <div ref={this.$refList} className={styles.list}>
+          {
+            list.map((item) => (
+              <div key={item.phone} className={styles.item} title={award.title}>
+                <div className={styles.left}>
+                  <Texty type="flash" mode="smooth" className={styles.name}>{item.name}</Texty>
+                  <Texty type="flash" mode="smooth" className={styles.awardTitle}>{award.name}</Texty>
+                </div>
+                <img src={award.image} alt={award.name} width={45} height={45} />
               </div>
-              <img src={award.image} alt={award.name} width={45} height={45} />
-            </div>
-          ))
-        }
-      </QueueAnim>
-      <div className={styles.opera}>
-        <button onClick={resetHandler}>重置</button>
-        <button onClick={exportHandler}>导出</button>
+            ))
+          }
+        </div>
+        <div className={styles.opera}>
+          <button onClick={resetHandler}>重置</button>
+          <button onClick={exportHandler}>导出</button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default memo(Winner);
+    );
+  }
+}
